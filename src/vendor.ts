3e518fafd89d8ecc4cc5aa94a546e8fd438606f4
SkyZH <iSkyZH@gmail.com>
2016-12-17 23:21:30 +0800

    update style

--- src/vendor.ts
+++ src/vendor.ts
@@ -12,6 +12,7 @@ import '@angularclass/hmr';
 import 'socket.io-client';
 
 import 'bootstrap/dist/css/bootstrap-flex.css';
+import 'font-awesome/css/font-awesome.css';
 
 // Other vendors for example jQuery, Lodash or Bootstrap
 // You can import js, ts, css, sass, ...
--- webpack.config.js
+++ webpack.config.js
@@ -98,8 +98,29 @@ module.exports = function makeWebpackConfig() {
 
       // copy those assets to output
       {
-        test: /\.(png|jpe?g|gif|svg|woff|woff2|ttf|eot|ico)(\?v=[0-9]\.[0-9]\.[0-9])?$/,
+        test: /\.(png|jpe?g|gif|ico)(\?v=[0-9]\.[0-9]\.[0-9])?$/,
         loader: 'file-loader?name=fonts/[name].[hash].[ext]?'
+      },
+
+      {
+        test: /\.woff(\?v=\d+\.\d+\.\d+)?$/,
+        loader: 'url-loader?limit=10000&mimetype=application/font-woff'
+      },
+      {
+        test: /\.woff2(\?v=\d+\.\d+\.\d+)?$/,
+        loader: 'url-loader?limit=10000&mimetype=application/font-woff'
+      },
+      {
+        test: /\.ttf(\?v=\d+\.\d+\.\d+)?$/,
+        loader: 'url-loader?limit=10000&mimetype=application/octet-stream'
+      },
+      {
+        test: /\.eot(\?v=\d+\.\d+\.\d+)?$/,
+        loader: 'file-loader'
+      },
+      {
+        test: /\.svg(\?v=\d+\.\d+\.\d+)?$/,
+        loader: 'url-loader?limit=10000&mimetype=image/svg+xml'
       },
 
       // Support for *.json files.
